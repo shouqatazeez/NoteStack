27f054c3e9f9335a6aa904655bcf6a70df7f22a7
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-09-08 00:12:08 +0530

    add the routes for the login and the signup in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-</div>
-  )
-}
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
