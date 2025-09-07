f291213ddee51e5ad2ff8b89d744ace72c0e2aed
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-09-08 00:28:54 +0530

    add the notes route in the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Notes from "./pages/Notes";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/notes" element={<Notes />} />
         </Routes>
       </div>
     </BrowserRouter>
