498ae81bac921b943666c1a88a18a24860a835ce
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-09-11 12:50:06 +0530

    some changes in nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,18 @@
 import { Button } from "@/components/ui/button";
-import logo from "@/components/Notelogo.png";
-import { Link } from "react-router";
+import logo from "@/assets/Nlogo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <header className="border-b px-4 md:px-6">
-      <div className="flex h-16 items-center justify-between gap-4">
+    <header className="fixed top-0 left-0 right-0 z-50 border-b px-4 md:px-6 shadow-md bg-white">
+      <div className="flex h-24 items-center justify-between gap-4">
         <div className="flex items-center gap-2">
           <div className="flex items-center gap-6">
             <a href="/" className="flex items-center gap-2">
               <img
                 src={logo}
                 alt="NoteStack"
-                className="h-25 w-auto object-contain bg-white"
+                className="h-16 md:h-20 lg:h-24 w-auto object-contain"
               />
             </a>
           </div>
@@ -22,7 +22,11 @@ function Navbar() {
           <Button asChild variant="ghost" size="sm" className="text-sm">
             <Link to={"/login"}>Sign In</Link>
           </Button>
-          <Button asChild size="sm" className="text-sm">
+          <Button
+            asChild
+            size="sm"
+            className="text-sm bg-[#0072FF] hover:bg-[#00BFFF]"
+          >
             <Link to={"/signup"}>Get Started</Link>
           </Button>
         </div>
