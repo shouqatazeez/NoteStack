969ee0bf65117074774d04ab712bef497658a101
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-09-15 23:44:35 +0530

    add features file

---
+++ src/pages/Features.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <div>
+      <section class="text-black-900  body-font">
+        <div class="container px-5 py-24 mx-auto ">
+          <div class="text-center mb-15">
+            <h1 class="text-4xl/tight font-bold tracking-tight xl:w-2/4 lg:w-3/4 mx-auto">
+              Key Features
+            </h1>
+          </div>
+          <div class="flex flex-wrap grid-rows-4 lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2 ">
+            <div class="p-2 sm:w-1/2 w-full">
+              <div class="bg-f5f5f5 rounded flex p-4 h-full items-center border border-black">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                  class="text-blue-600 w-6 h-6 flex-shrink-0 mr-4"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                  <path d="M22 4L12 14.01l-3-3"></path>
+                </svg>
+                <span class="title-font font-medium text-black">
+                  Simple Notetaking
+                </span>
+              </div>
+            </div>
+            <div class="p-2 sm:w-1/2 w-full">
+              <div class="bg-f5f5f5 rounded flex p-4 h-full items-center border border-black">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                  class="text-blue-600 w-6 h-6 flex-shrink-0 mr-4"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                  <path d="M22 4L12 14.01l-3-3"></path>
+                </svg>
+                <span class="title-font font-medium text-black">Secure</span>
+              </div>
+            </div>
+            <div class="p-2 sm:w-1/2 w-full">
+              <div class="bg-f5f5f5 rounded flex p-4 h-full items-center border border-black">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                  class="text-blue-600 w-6 h-6 flex-shrink-0 mr-4"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                  <path d="M22 4L12 14.01l-3-3"></path>
+                </svg>
+                <span class="title-font font-medium text-black">
+                  Easy to Use
+                </span>
+              </div>
+            </div>
+            <div class="p-2 sm:w-1/2 w-full">
+              <div class="bg-f5f5f5 rounded flex p-4 h-full items-center border border-black">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                  class="text-blue-600 w-6 h-6 flex-shrink-0 mr-4"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                  <path d="M22 4L12 14.01l-3-3"></path>
+                </svg>
+                <span class="title-font font-medium text-black">
+                  Fast & Reliable
+                </span>
+              </div>
+            </div>
+            <div class="p-2 sm:w-1/2 w-full">
+              <div class="bg-f5f5f5 rounded flex p-4 h-full items-center border border-black">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                  class="text-blue-600 w-6 h-6 flex-shrink-0 mr-4"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                  <path d="M22 4L12 14.01l-3-3"></path>
+                </svg>
+                <span class="title-font font-medium text-blac">
+                  Personalized Dashboard
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Features;
